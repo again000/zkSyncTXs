442c0cdce7b618f64a15c63956112f0b10250473
again000 <againwithzero@gmail.com>
2023-07-27 00:23:27 +0300

    changes and upgrades

--- functions/functions.py
+++ functions/functions.py
@@ -19,15 +19,19 @@ def deposit(min_val, max_val, pvt_key):
 		'gasPrice': web3.to_wei(0.25, 'gwei'),
 		'nonce': web3.eth.get_transaction_count(web3.to_checksum_address(address))
 	})
-
 	transaction['gas'] = int(web3.eth.estimate_gas(transaction))
 	signed_txn = web3.eth.account.sign_transaction(transaction, pvt_key)
 	transaction_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction).hex()
 	random_sleep()
 	return print(f"Deposited {value_eth} ETH | Hash: {transaction_hash}")
 
-def deposit_swap(value_wei,pvt_key):
+def deposit_swap(keep_value_from,keep_value_to,pvt_key):
 	address = web3.eth.account.from_key(pvt_key).address
+	balance = check_balance_wallet(pvt_key)			
+	value = dc.Decimal.from_float(float("0.0000" + str(random.randrange(keep_value_from,keep_value_to))))
+	# balance minus value
+	value_eth = "{:.8f}".format(balance - value)   
+	value_wei = web3.to_wei(value_eth, 'ether')
 	transaction = contract.functions.deposit().build_transaction({
 		'from': web3.to_checksum_address(address),
 		'value': value_wei,
@@ -41,7 +45,9 @@ def deposit_swap(value_wei,pvt_key):
 	return print(f"Deposited {value_eth} ETH | Hash: {transaction_hash}")
 
 def withdraw(pvt_key):
+	web3 = Web3(Web3.HTTPProvider(RPC))
 	address = web3.eth.account.from_key(pvt_key).address
+	contract = web3.eth.contract(contract_address, abi=contract_abi)
 	balance = contract.functions.getBalance().call({'from': address})
 	transaction = contract.functions.withdraw(
 		balance
@@ -57,9 +63,23 @@ def withdraw(pvt_key):
 	random_sleep()
 	return print(f"Withdrawing {web3.from_wei(balance, 'ether')} ETH for {address}\nHash: {transaction_hash}\nPrivate key: {pvt_key}")
 
-def withdraw_swap(value_wei, pvt_key):
-	address = web3.eth.account.from_key(pvt_key).address
-	balance = contract.functions.getBalance().call({'from': address})
+def withdraw_swap(keep_value_from, keep_value_to, pvt_key):
+	contract_balance = check_balance(pvt_key)
+	value = dc.Decimal.from_float(float("0.0000" + str(random.randrange(keep_value_from,keep_value_to))))
+	# balance minus value
+	value_eth = "{:.8f}".format(contract_balance - value)
+	balance = float(value_eth)
+	if balance < 0:
+		balance == 0
+		raise Exception("Resulting wei value must be between 1 and 2**256 - 1")
+	elif balance > 0:
+		balance	
+		try:
+			balance = web3.to_wei(balance, 'ether')
+		except Exception as e:
+			print(f"""Wallet: {web3.eth.account.from_key(key).address} has a lower ballance. Error message: {e}""")
+	else:
+		pass
 	transaction = contract.functions.withdraw(
 		balance
 	).build_transaction({
--- zk.py
+++ zk.py
@@ -29,19 +29,14 @@ def main():
 		try:
 			if choice == 1:
 				if 		swap_all_balance == True:
-					balance = check_balance_wallet(key)			
-					value = dc.Decimal.from_float(float("0.0000" + str(random.randrange(keep_value_from,keep_value_to))))
-					# balance minus value
-					value_eth = "{:.8f}".format(balance - value)   
-					value_wei = web3.to_wei(value_eth, 'ether')
-					deposit_swap(value_wei,pvt_key)
+					deposit_swap(keep_value_from,keep_value_to,key)
 				elif 	swap_all_balance == False:
 					deposit(eth_min, eth_max, key)
 				else:
 					print("Something wrong with settings.py")
 			elif choice == 2:
-				if 		swap_all_balance == True:
-					withdraw(key)
+				if 		swap_all_balance == True:   
+					withdraw_swap(keep_value_from, keep_value_to, key)
 				elif	swap_all_balance == False:
 					withdraw(key)
 				else:
